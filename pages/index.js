54fca14e4b7cb9cfbe226254268b0958bf3a73ad
Benedict <benedictryan80@gmail.com>
2022-05-12 00:28:33 +0700

    edit Home Title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
 		<div className={styles.container}>
 			<Header />
 			<main className={styles.main}>
-				<h1 className={styles.title}>Home page</h1>
+				<h1 className={styles.title}>Multi Zone Home-Page</h1>
 			</main>
 		</div>
 	);
